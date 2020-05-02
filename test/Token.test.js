0fb05e9bdd92d444f1ede974c4d270d5df472da7
fjhansen <58237856+fjhansen@users.noreply.github.com>
2020-05-01 19:13:03 -0700

    unit tests complete and passing

--- test/Token.test.js
+++ test/Token.test.js
@@ -5,15 +5,31 @@ require('chai')
 .should()
 
 contract('Token', (accounts) => {
+  const name = 'Juniper'
+  const symbol = 'JUNI'
+  const decimals = '18'
+  const totalSupply = '1000000000000000000000000'
   let token
   beforeEach(async () => {
-    token = await Token.new()
-    
+  token = await Token.new()
   })
   describe('deployment', () => {
     it('tracks the name in deploy', async () => {
       const result = await token.name()
-      result.should.equal('My Name')
+      result.should.equal(name)
+    })
+    it('tracks the symbol', async () => {
+      const result = await token.symbol()
+      result.should.equal(symbol)
+    })
+    it('tracks the decimals', async () => {
+      const result = await token.decimals()
+      result.toString().should.equal(decimals)
+    })
+
+    it('tracks the total supply', async () => {
+      const result = await token.totalSupply()
+      result.toString().should.equal(totalSupply)
     })
   })
 })
